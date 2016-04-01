b3764e5d06306cd03e82fbbb4318650afd9c8749
daniel-sz <danielszewczy@gmail.com>
2016-04-01 10:53:30 +0200

    Rozwiązanie

--- main.cpp
+++ main.cpp
@@ -7,17 +7,36 @@ void drawCharSquare(char **square, int n);
 
 int main(int argc, char **argv)
 {
-	int n;
+	int n=atoi(argv[1]);
+	srand(argc==3?atoi(argv[2]):time(NULL));
 	drawCharSquare(charSquare(n), n);	
 }
 
 char **charSquare(int n)
 {
-	char **square;
+	char **square = new char*[n];
+	
+	for (int i = 0; i < n; ++i)
+	{
+		square[i] = new char [n];
+	}
+	for (int i = 0; i < (n*n); ++i)
+	{
+		int x=i/n, y=i%n;
+		square[x][y]='a'+rand()%('z'-'a');
+	}
 	return square;
 }
 
 void drawCharSquare(char **square, int n)
 {
-
+	char z;
+	for (int i = 0; i < (n*n); ++i)
+	{
+		int x=i/n, y=i%n;
+		(i%n)==(n-1)?z=13:z=' ';
+		printf("%c%c",square[x][y],y==n-1?10:32);  //Na windowsie to nie zadziała, bo: https://goo.gl/VpES74 ale wtedy można zamiast %c użyć %s i wstawić "\n"
+	}
+	for(int i=0;i<n;i++) delete [] square[i];
+	delete [] square;
 }
